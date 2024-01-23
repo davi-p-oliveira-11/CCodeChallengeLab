b3116509b9e68fe2d77896aacdcc9c33369b8417
Davi P. Oliveira <dpo.dev11@gmail.com>
2024-01-23 12:44:53 -0300

    Create solution.c

---
+++ Challenges/BRL-to-USD/solution.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+
+  float avaiableBRL, usdValue, affordableUsd;
+
+  printf("Enter a value in BRL: ");
+  scanf("%f", &avaiableBRL);
+  getchar();
+
+  printf("What is today's dollar exchange rate ? ");
+  scanf("%f", &usdValue);
+  getchar();
+
+  affordableUsd = avaiableBRL / usdValue;
+
+  printf("You can buy a total of %.2f USD" , affordableUsd);
+  getchar();
+
+  printf("Press Enter to Exit ... ");
+  getchar();
+
+
+  return 0;
+}
