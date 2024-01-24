e7ac7e9420fcb4e0b8935db3e02b9ef0dcd79828
Davi P. Oliveira <dpo.dev11@gmail.com>
2024-01-24 16:33:41 -0300

    Update solution.c

--- Challenges/HelloUser/solution.c
+++ Challenges/HelloUser/solution.c
@@ -1,13 +1,21 @@
 #include <stdio.h>
+#include <string.h>
 
 int main() {
   char personName[100];
 
-  printf("What is yout name ? ");
-  scanf("%s", &personName);
-  getchar();
+  printf("What is you name ? ");
+  fgets(personName, sizeof(personName), stdin);
+
+  // Remove the newline character from personName
+    int length = strlen(personName);
+
+    if (personName[length - 1] == '\n') {
+        personName[length - 1] = '\0';
+    }
 
   printf("Hello, %s nice to meet you ! \n", personName);
+  printf("Press Enter to Exit ...");
   getchar();
   
   return 0;
